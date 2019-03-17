f06f8e40b1b2cff23f3e0d5c430367a4e6710af4
Dzmitry Kosmach <dzmitry.kosmach@gmail.com>
2019-03-17 23:01:12 +0300

    fix

--- src/main/java/by/training/jenkins/Runner.java
+++ src/main/java/by/training/jenkins/Runner.java
@@ -3,5 +3,6 @@ package by.training.jenkins;
 public class Runner {
     public static void main (String[] args){
         System.out.println("Hello Jenkins!!!");
+        System.out.println("");
     }
 }
